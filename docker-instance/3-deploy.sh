7937b69f65a91fbe9f1044aab76ce959f06b4c57
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2024-05-27 18:55:53 -0500

    improvement

--- docker-instance/3-deploy.sh
+++ docker-instance/3-deploy.sh
@@ -1,9 +1,9 @@
 ###################################################################################################
-#### nginx ########################################################################################
+#### deploy #######################################################################################
 ###################################################################################################
 
 # if "docker ps --format=json | jq .Ports" has 4 "8080"s, then we are currently on port 8080
-function nginx_next_proxy_port {
+function find_next_proxy_port {
   port_8080_count=$(docker ps --format=json | jq .Ports | grep -Fo 8080 | wc -l)
 
   if [ $port_8080_count == "0" ]; then
@@ -17,27 +17,21 @@ function nginx_next_proxy_port {
   fi
 }
 
-###################################################################################################
-#### deployment ###################################################################################
-###################################################################################################
-
 function deploy {
   local docker_username=$1
   local docker_password=$2
   local docker_image_repository=$3
   local docker_image_tag=$4
+
   local docker_tag=$docker_username/$docker_image_repository:$docker_image_tag
 
-  echo "logging into docker"
   docker login --username=$docker_username --password=$docker_password
-
-  echo "pulling tag: $docker_tag"
   docker pull $docker_tag
 
   docker_current_ps=$(docker ps -q)
   echo "found existing container ids: $docker_current_ps"
 
-  next_port=$(nginx_next_proxy_port)
+  next_port=$(find_next_proxy_port)
   echo "found next port: $next_port"
 
   if [ $next_port == "unknown" ]; then
@@ -45,17 +39,14 @@ function deploy {
     return 1
   fi
 
-  echo "attempting to run new image tag"
   docker run -d -p $next_port:8080 --restart=always $docker_tag || exit 1
 
-  echo "switching nginx config and reloading"
-  sudo cp nginx-$next_port.conf /etc/nginx/nginx.conf
-  sudo nginx -s reload
-
-  echo "attempting to stop and terminate old containers"
   if [ "$docker_current_ps" ]; then
+    echo "attempting to stop and terminate old containers"
     docker stop $docker_current_ps
     docker rm $docker_current_ps
+  else
+    echo "no old containers found"
   fi
 }
 
