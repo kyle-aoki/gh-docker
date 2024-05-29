10629b327f09c83c4509703d6910496bdb15bbe1
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2024-05-29 18:29:05 -0500

    switch to app instead of script

---
+++ docker.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/api/types/registry"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+func dockerclient() *client.Client {
+	return must(client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation()))
+}
+
+func dockerauth() string {
+	authConfig := registry.AuthConfig{Username: CFG.DockerUsername, Password: CFG.DockerPassword}
+	authBytes := must(json.Marshal(authConfig))
+	authBase64 := base64.URLEncoding.EncodeToString(authBytes)
+	return authBase64
+}
+
+func listContainers() []types.Container {
+	log.Println("listing containers")
+	return must(dockerclient().ContainerList(context.Background(), container.ListOptions{All: true}))
+}
+
+func deploy(tag string) []types.Container {
+	ctx := context.Background()
+
+	imageName := fmt.Sprintf("%s/%s:%s", CFG.DockerUsername, CFG.DockerRepository, tag)
+	log.Println("deploying", imageName)
+
+	cli := dockerclient()
+	defer cli.Close()
+
+	log.Println("pulling", imageName)
+	out := must(cli.ImagePull(ctx, imageName, image.PullOptions{RegistryAuth: dockerauth()}))
+	defer out.Close()
+	must(io.Copy(io.Discard, out))
+	log.Println("✅ pulled", imageName)
+
+	containers := must(cli.ContainerList(ctx, container.ListOptions{All: true}))
+
+	var nextPort string = "8080"
+	var shouldKillOneContainer bool
+	var containerToKill types.Container
+
+	if len(containers) == 1 {
+		cont := containers[0]
+		for _, port := range cont.Ports {
+			if port.PublicPort == 8080 || port.PublicPort == 8081 {
+				containerToKill = cont
+				shouldKillOneContainer = true
+				if port.PublicPort == 8080 {
+					nextPort = "8081"
+				} else {
+					nextPort = "8080"
+				}
+			}
+		}
+	} else if len(containers) > 1 {
+		killAll(cli, ctx, containers)
+	}
+
+	cr := must(cli.ContainerCreate(
+		ctx,
+		&container.Config{
+			Image:        imageName,
+			ExposedPorts: nat.PortSet{"8080/tcp": {}},
+		},
+		&container.HostConfig{
+			PortBindings: nat.PortMap{
+				"8080/tcp": []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: nextPort}},
+			},
+		},
+		&network.NetworkingConfig{},
+		v1.DescriptorEmptyJSON.Platform,
+		"",
+	))
+	log.Println("created container:", cr.ID)
+
+	check(cli.ContainerStart(ctx, cr.ID, container.StartOptions{}))
+	log.Println("started container", cr.ID)
+
+	if shouldKillOneContainer {
+		killAll(cli, ctx, []types.Container{containerToKill})
+	}
+
+	return must(cli.ContainerList(ctx, container.ListOptions{All: true}))
+}
+
+func killAll(cli *client.Client, ctx context.Context, containers []types.Container) {
+	for _, cont := range containers {
+		log.Println("killing container:", cont.ID)
+		check(cli.ContainerStop(ctx, cont.ID, container.StopOptions{}))
+		check(cli.ContainerRemove(ctx, cont.ID, container.RemoveOptions{
+			RemoveVolumes: true,
+			Force:         true,
+		}))
+	}
+}
--- petra.go
+++ petra.go
@@ -1,137 +1,60 @@
 package main
 
 import (
-	"context"
-	"encoding/base64"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/api/types/registry"
-	"github.com/docker/docker/client"
-	"github.com/docker/go-connections/nat"
-	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-var (
-	username    = flag.String("username", "", "dockerhub username")
-	password    = flag.String("password", "", "dockerhub password or token")
-	repository  = flag.String("repository", "", "dockerhub repository")
-	tag         = flag.String("tag", "", "image tag")
-	isDebugMode = flag.Bool("debug", false, "debug mode")
-)
+type PetraConfig struct {
+	DockerUsername   string
+	DockerPassword   string
+	DockerRepository string
+	Key              string
+}
+
+var CFG *PetraConfig = &PetraConfig{}
 
 func main() {
-	flag.Parse()
-	ctx := context.Background()
-
-	if !*isDebugMode {
-		defer func() {
-			if r := recover(); r != nil {
-				fmt.Println(r)
-				os.Exit(1)
-			}
-		}()
+	args := os.Args[1:]
+	if len(args) != 1 {
+		panic("petra <path-to-config-file.json>")
 	}
+	bytes := must(os.ReadFile(args[0]))
+	check(json.Unmarshal(bytes, &CFG))
 
-	if *username == "" || *password == "" || *repository == "" || *tag == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+	http.HandleFunc("/deploy", httpDeploy)
+	http.HandleFunc("/list", httpList)
 
-	authConfig := registry.AuthConfig{Username: *username, Password: *password}
-	authBytes := must(json.Marshal(authConfig))
-	authBase64 := base64.URLEncoding.EncodeToString(authBytes)
+	am := &AuthMiddleware{handler: http.DefaultServeMux}
 
-	imageName := fmt.Sprintf("%s/%s:%s", *username, *repository, *tag)
-	log.Println("deploying", imageName)
+	log.Println("listening on port 10000")
+	check(http.ListenAndServe("0.0.0.0:10000", am))
+}
 
-	cli := must(client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation()))
-	defer cli.Close()
-
-	log.Println("pulling", imageName)
-	out := must(cli.ImagePull(ctx, imageName, image.PullOptions{RegistryAuth: authBase64}))
-	defer out.Close()
-	must(io.Copy(io.Discard, out))
-	log.Println("✅ pulled", imageName)
-
-	containers := must(cli.ContainerList(ctx, container.ListOptions{All: true}))
-
-	var nextPort string = "8080"
-	var shouldKillOneContainer bool
-	var containerToKill types.Container
-
-	if len(containers) == 1 {
-		cont := containers[0]
-		for _, port := range cont.Ports {
-			if port.PublicPort == 8080 || port.PublicPort == 8081 {
-				containerToKill = cont
-				shouldKillOneContainer = true
-				if port.PublicPort == 8080 {
-					nextPort = "8081"
-				} else {
-					nextPort = "8080"
-				}
-			}
-		}
-	} else if len(containers) > 1 {
-		kill(cli, ctx, containers[0])
-	}
-
-	cr := must(cli.ContainerCreate(
-		ctx,
-		&container.Config{
-			Image:        imageName,
-			ExposedPorts: nat.PortSet{"8080/tcp": {}},
-		},
-		&container.HostConfig{
-			PortBindings: nat.PortMap{
-				"8080/tcp": []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: nextPort}},
-			},
-		},
-		&network.NetworkingConfig{},
-		v1.DescriptorEmptyJSON.Platform,
-		"",
-	))
-	log.Println("created container:", cr.ID)
-
-	check(cli.ContainerStart(ctx, cr.ID, container.StartOptions{}))
-	log.Println("started container", cr.ID)
-
-	if shouldKillOneContainer {
-		killAll(cli, ctx, []types.Container{containerToKill})
+func handleHttpPanic(w http.ResponseWriter) {
+	if r := recover(); r != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("%s", r)))
 	}
 }
 
-func killAll(cli *client.Client, ctx context.Context, containers []types.Container) {
-	for _, cont := range containers {
-		kill(cli, ctx, cont)
+func httpDeploy(w http.ResponseWriter, r *http.Request) {
+	type DeployInput struct {
+		Tag string
 	}
+	defer handleHttpPanic(w)
+	bytes := must(io.ReadAll(r.Body))
+	input := &DeployInput{}
+	check(json.Unmarshal(bytes, &input))
+	containers := deploy(input.Tag)
+	w.Write(must(json.Marshal(containers)))
 }
 
-func kill(cli *client.Client, ctx context.Context, cont types.Container) {
-	log.Println("killing container:", cont.ID)
-	check(cli.ContainerStop(ctx, cont.ID, container.StopOptions{}))
-	check(cli.ContainerRemove(ctx, cont.ID, container.RemoveOptions{
-		RemoveVolumes: true,
-		Force:         true,
-	}))
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func must[T any](t T, err error) T {
-	check(err)
-	return t
+func httpList(w http.ResponseWriter, r *http.Request) {
+	defer handleHttpPanic(w)
+	w.Write(must(json.Marshal(listContainers())))
 }
