9fa8596003fd0af73d2b1fb9923457b11a4d6c3e
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2024-05-29 16:15:42 -0500

    use golang script instead of bash

--- docker-instance-postgres.sh
+++ docker-instance-postgres.sh
@@ -3,6 +3,8 @@
 sudo apt install postgresql-client-common -y
 sudo apt install postgresql-client -y
 
+current_date=$(date '+%Y_%m_%d_%H_%M')
+
 PGPASSWORD="hunter2" pg_dump -h localhost -p 5432 -U postgres --column-inserts postgres > pg-backup.sql
 
 # operations
